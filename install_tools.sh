08a07186875cc8e78b7b7e56c1ff89feabd20221
adam <adamsmithwwwhk@gmail.com>
2018-01-24 00:59:15 -0500

    add nginx install

--- install_tools.sh
+++ install_tools.sh
@@ -1,9 +1,32 @@
 #!/bin/sh
-yum -y install coreutils
-yum -y install net-tools
+
+base() {
+    yum -y install coreutils
+    yum -y install net-tools
+}
 
 #install shadowssocks
-yum -y install python-setuptools && easy_install pip
-pip install shadowsocks
-cp ./conf/shadowsocks.json /etc/
-ssserver -c /etc/shadowsocks.json -d start
+shadowsocks() {
+    yum -y install python-setuptools && easy_install pip
+    pip install shadowsocks
+    cp ./conf/shadowsocks.json /etc/
+    ssserver -c /etc/shadowsocks.json -d start
+}
+
+#better top command
+htop() {
+    yum -y install epel-release
+    yum -y update
+    yum -y install htop
+}
+
+nginx() {
+    sudo yum -y install epel-release
+    sudo yum -y install nginx
+
+    sudo systemctl start nginx
+
+    sudo firewall-cmd --permanent --zone=public --add-service=http 
+    sudo firewall-cmd --permanent --zone=public --add-service=https
+    sudo firewall-cmd --reload
+}
