bcfbbb2cd790ebcc42d7b48f31dbf4c3ba12dc49
adam <adamsmithwwwhk@gmail.com>
2018-01-24 11:59:43 +0000

    1.add nginx install 2.add bench script

--- install_tools.sh
+++ install_tools.sh
@@ -1,23 +1,64 @@
 #!/bin/sh
 
+declare -a PACKAGES=(base) 
+
+
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+case $key in
+    -p|--pkg)
+    INPUT_PACKAGES=()
+    INPUT_PACKAGES+=("$2")
+    shift # past argument
+    while [[ -n $2 ]] && ! [[ $2 == "-"* ]]
+    do
+    INPUT_PACKAGES+=("$2")
+    shift # past value
+
+    done
+    shift # past value
+    ;;
+    *)    # unknown option
+    POSITIONAL+=("$1") # save it in an array for later
+    shift # past argument
+    ;;
+esac
+done
+
+
 base() {
-    yum -y install coreutils
-    yum -y install net-tools
+    sudo yum -y install coreutils
+    sudo yum -y install net-tools
 }
 
 #install shadowssocks
 shadowsocks() {
-    yum -y install python-setuptools && easy_install pip
-    pip install shadowsocks
-    cp ./conf/shadowsocks.json /etc/
-    ssserver -c /etc/shadowsocks.json -d start
+    sudo yum -y install python-setuptools  
+    sudo easy_install pip
+
+
+    sudo pip install shadowsocks
+    sudo cp ./conf/shadowsocks.json /etc/
+    sudo ssserver -c /etc/shadowsocks.json -d start
+
+    sudo firewall-cmd --add-port=10000/tcp
+    sudo firewall-cmd --permanent --add-port=10000/tcp
+
+    sudo firewall-cmd --add-port=10001/tcp
+    sudo firewall-cmd --permanent --add-port=10001/tcp
+
+    sudo firewall-cmd --add-port=10002/tcp
+    sudo firewall-cmd --permanent --add-port=10002/tcp
 }
 
 #better top command
 htop() {
-    yum -y install epel-release
-    yum -y update
-    yum -y install htop
+    sudo yum -y install epel-release
+    sudo yum -y update
+    sudo yum -y install htop
 }
 
 nginx() {
@@ -25,8 +66,22 @@ nginx() {
     sudo yum -y install nginx
 
     sudo systemctl start nginx
+    sudo systemctl enable nginx
 
     sudo firewall-cmd --permanent --zone=public --add-service=http 
     sudo firewall-cmd --permanent --zone=public --add-service=https
     sudo firewall-cmd --reload
+
 }
+
+INSTALL_PACKAGES=${PACKAGES[@]}
+if ! [ -z ${INPUT_PACKAGES+x} ]; then
+    INSTALL_PACKAGES=${INPUT_PACKAGES[@]}
+fi
+
+echo "INSTALL_PACKAGES:${INSTALL_PACKAGES[@]}"
+
+for each_pkg in ${INSTALL_PACKAGES[@]}
+do
+    $each_pkg
+done
